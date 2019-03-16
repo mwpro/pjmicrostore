adb30ca321db691a25a8141d94f9fc10a2ea43a6
Maciej Wódke <maciej@mwpro.pl>
2019-03-16 19:21:28 +0100

    Add consumer to Checkout.Cart

---
+++ src/Checkout/Checkout.Cart/Consumers/OrderPlacedConsumer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Checkout.Orders.Controllers;
+using MassTransit;
+
+namespace Checkout.Orders.Controllers
+{
+    public class OrderPlacedEvent
+    {
+        public int OrderId { get; set; }
+        public int SourceCartId { get; set; }
+    }
+}
+
+namespace Checkout.Cart.Consumers
+{
+    public class OrderPlacedConsumer : IConsumer<OrderPlacedEvent>
+    {
+        public Task Consume(ConsumeContext<OrderPlacedEvent> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Checkout/Checkout.Cart/Startup.cs
+++ src/Checkout/Checkout.Cart/Startup.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Checkout.Cart.Consumers;
 using Checkout.Cart.Domain;
 using Checkout.Cart.Services;
+using Checkout.Orders.Controllers;
+using GreenPipes;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,8 +16,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace Checkout.Cart
 {
@@ -34,7 +41,28 @@ namespace Checkout.Cart
             services.AddDbContext<CartContext>
                 (options => options.UseSqlServer(connectionString));
 
-            services.AddTransient<IProductsService, ProductsService>();
+            services.AddMassTransit(c =>
+            {
+                c.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(
+                    cfg =>
+                    {
+                        cfg.Host("localhost", "/", h => { });
+                        cfg.ReceiveEndpoint("Checkout.Cart", e =>
+                        {
+                            //e.PrefetchCount = 16;
+                            e.UseMessageRetry(x => x.Interval(2, 100));
+
+                            e.ConfigureConsumer<OrderPlacedConsumer>(provider);
+
+                            EndpointConvention.Map<OrderPlacedEvent>(e.InputAddress);
+                        });
+                    }));
+                c.AddConsumer<OrderPlacedConsumer>();
+            });
+            services.AddSingleton<IBus>(provider => provider.GetRequiredService<IBusControl>());
+            services.AddTransient<IProductsService, ProductsService>(); // todo scoped or transient?
+            services.AddTransient<OrderPlacedConsumer>();
+            services.AddSingleton<IHostedService, CartService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +79,27 @@ namespace Checkout.Cart
 
             app.UseHttpsRedirection();
             app.UseMvc();
+        }
+    }
+
+    public class CartService :
+        IHostedService
+    {
+        private readonly IBusControl _busControl;
+
+        public CartService(IBusControl busControl)
+        {
+            _busControl = busControl;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return _busControl.StartAsync(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return _busControl.StopAsync(cancellationToken);
         }
     }
 }

72c7adedb6bb63206cca6a230f5e95dafa04a48e
Maciej Wódke <maciej@mwpro.pl>
2019-04-02 22:26:13 +0200

    Remove buefy, add bootstrap to front

--- front-store/src/layouts/DefaultLayout.vue
+++ front-store/src/layouts/DefaultLayout.vue
@@ -7,27 +7,7 @@
           </div>
         </section>
         <footer class="footer has-text-centered">
-          <div class="container">
-             <div class="columns">
-              <div class="column is-8-desktop is-offset-2-desktop">
-                <p>
-                  <strong class="has-text-weight-semibold">
-                    <a href="https://www.npmjs.com/package/bulma-start">bulma-start@0.0.3</a>
-                  </strong>
-                </p>
-                <p>
-                  <small>
-                    Source code licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>
-                  </small>
-                </p>
-                <p style="margin-top: 1rem;">
-                  <a href="http://bulma.io">
-                    <img src="made-with-bulma.png" alt="Made with Bulma" width="128" height="24">
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
+          pjmicrostore 2019
         </footer>
     </div>
 </template>
--- front-store/src/main.js
+++ front-store/src/main.js
@@ -2,12 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
+import BootstrapVue from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
-
-Vue.use(Buefy)
 
 Vue.filter('currency', (price) => `${price != null ? price.toFixed(2) : "0,00"} PLN`);
 Vue.filter('date', date => new Date(date).toLocaleDateString());

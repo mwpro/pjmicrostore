97af99b707352b602f6951e8f118e5665d855cef
Maciej Wódke <maciej@mwpro.pl>
2019-04-24 02:46:06 +0200

    Remove sidebar from homepage

--- front-store/src/components/TopMenu.vue
+++ front-store/src/components/TopMenu.vue
@@ -48,10 +48,6 @@
           >
             {{ category.name }}
           </router-link>
-          <a class="p-2 text-muted" href="#">World</a>
-          <a class="p-2 text-muted" href="#">U.S.</a>
-          <a class="p-2 text-muted" href="#">Technology</a>
-          <a class="p-2 text-muted" href="#">Design</a>
         </nav>
       </div>
 
@@ -88,5 +84,7 @@ export default {
 </script>
 
 <style>
-
+  .router-link-active {
+    font-weight: bold;
+  }
 </style>
--- front-store/src/pages/Home.vue
+++ front-store/src/pages/Home.vue
@@ -14,32 +14,6 @@
 
     <main role="main" class="container">
       <div class="row">
-        <aside class="col-md-3 blog-sidebar">
-          <CategoriesList />
-          <SearchFilters />
-          <div class="p-4 mb-3 bg-light rounded">
-            <h4 class="font-italic">About</h4>
-            <p class="mb-0">
-              Etiam porta
-              <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.
-            </p>
-          </div>
-
-          <div class="p-4">
-            <h4 class="font-italic">Elsewhere</h4>
-            <ol class="list-unstyled">
-              <li>
-                <a href="#">GitHub</a>
-              </li>
-              <li>
-                <a href="#">Twitter</a>
-              </li>
-              <li>
-                <a href="#">Facebook</a>
-              </li>
-            </ol>
-          </div>
-        </aside>
         <div class="col blog-main">
           <div class="row">
             <ProductBox v-for="product in products" v-bind:key="product.id" v-bind:product="product"/>

84f672c6e4f885a41ac9f38248991e55f42715a0
Maciej Wódke <maciej@mwpro.pl>
2019-04-27 15:50:23 +0200

    Add simple api gateway for front store

--- front-store/vue.config.js
+++ front-store/vue.config.js
@@ -1,32 +1,8 @@
 module.exports = {
   devServer: {
     proxy: {
-      '/api/products/.*/photos': {
-        target: 'http://localhost:58120',
-        changeOrigin: true,
-      },
-      '/api/products': {
-        target: 'http://localhost:53606',
-        changeOrigin: true,
-      },
-      '/api/categories': {
-        target: 'http://localhost:53606',
-        changeOrigin: true,
-      },
-      '/api/cart': {
-        target: 'http://localhost:64642',
-        changeOrigin: true,
-      },
-      '/api/orders': {
-        target: 'http://localhost:56037',
-        changeOrigin: true,
-      },
-      '/api/payments': {
-        target: 'http://localhost:62678',
-        changeOrigin: true,
-      },
-      '/api/search': {
-        target: 'http://localhost:52910',
+      '/': {
+        target: 'http://localhost:64646',
         changeOrigin: true,
       },
     },
---
+++ src/External/External.FrontApiGateway/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.ServiceDiscovery;
+using Ocelot.ServiceDiscovery.Providers;
+using Ocelot.Values;
+
+namespace External.FrontApiGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, builder) =>
+                    {
+                        builder.AddJsonFile(Path.Combine("configuration", "gateway.json"), false, true)
+                            .AddJsonFile(Path.Combine("configuration", $"gateway.{hostingContext.HostingEnvironment.EnvironmentName}.json"), true, true);
+                    })
+                .ConfigureServices((webHost, s) =>
+                {
+                    s.AddOcelot();
+                    s.AddGatewayServicesFromConfiguration();
+                })
+                .Configure(app =>
+                {
+                    app.UseOcelot().Wait();
+                });
+    }
+
+    public class GatewayServicesConfiguration
+    {
+        public List<GatewayServiceConfiguration> GatewayServices { get; set; }
+
+        public class GatewayServiceConfiguration
+        {
+            public string Name { get; set; }
+            public string DownstreamHost { get; set; }
+            public int DownstreamPort { get; set; }
+        }
+    }
+
+    public static class GatewayServicesFromConfigurationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddGatewayServicesFromConfiguration(this IServiceCollection services)
+        {
+            var service = services.First(x => x.ServiceType == typeof(IConfiguration));
+            var configuration = (IConfiguration)service.ImplementationInstance;
+
+            var servicesConfiguration = configuration.Get<GatewayServicesConfiguration>();
+            var gatewayServices = servicesConfiguration.GatewayServices.Select(x =>
+                    new Service(x.Name, new ServiceHostAndPort(x.DownstreamHost, x.DownstreamPort), string.Empty,
+                        string.Empty, new string[0])).ToList();
+
+            services.AddSingleton<ServiceDiscoveryFinderDelegate>((provider, config, key) => new ConfigurationServiceProvider(gatewayServices));
+            return services;
+        }
+    }
+}

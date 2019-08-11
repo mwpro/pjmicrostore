cfaec58834f881b7307178fe46ef0c59eba21709
Maciej Wódke <maciej@mwpro.pl>
2019-08-11 22:32:45 +0200

    Bump search version

--- release-all.ps1
+++ release-all.ps1
@@ -45,7 +45,7 @@ $DOCKER_IMAGE_VERSION = "1.0.0"
 docker tag $DOCKER_REPOSITORY_NAME/pjmicrostore-products-catalog $DOCKER_REPOSITORY_NAME/pjmicrostore-products-catalog:$DOCKER_IMAGE_VERSION
 docker push $DOCKER_REPOSITORY_NAME/pjmicrostore-products-catalog:$DOCKER_IMAGE_VERSION
 docker push $DOCKER_REPOSITORY_NAME/pjmicrostore-products-catalog:latest
-$DOCKER_IMAGE_VERSION = "1.0.4"
+$DOCKER_IMAGE_VERSION = "1.0.5"
 docker tag $DOCKER_REPOSITORY_NAME/pjmicrostore-products-search $DOCKER_REPOSITORY_NAME/pjmicrostore-products-search:$DOCKER_IMAGE_VERSION
 docker push $DOCKER_REPOSITORY_NAME/pjmicrostore-products-search:$DOCKER_IMAGE_VERSION
 docker push $DOCKER_REPOSITORY_NAME/pjmicrostore-products-search:latest

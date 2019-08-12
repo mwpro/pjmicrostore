d15e4c842de1fafa721a4c4d1bae44ac3ae85ef2
Maciej Wódke <maciej@mwpro.pl>
2019-08-12 22:45:30 +0200

    Store working on AKS

--- azure/3_Databases.ps1
+++ azure/3_Databases.ps1
@@ -6,6 +6,8 @@ Write-Host "Using password $sqlPassword"
 
 az sql server create -g pjmicrostore -n pjmicrostore-sql --location westeurope -u $sqlUserName -p "$sqlPassword"
 
+az sql server firewall-rule create -g pjmicrostore -s pjmicrostore-sql -n allow-azure-services --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
+
 az sql elastic-pool create --name pjmicrostore-sql-sp --resource-group pjmicrostore --server pjmicrostore-sql --edition Basic --capacity 50
 
 az sql db create --name Checkout.Cart --resource-group pjmicrostore --server pjmicrostore-sql --elastic-pool pjmicrostore-sql-sp
@@ -15,7 +17,6 @@ az sql db create --name Checkout.Orders --resource-group pjmicrostore --server p
 az sql db create --name Checkout.Payments --resource-group pjmicrostore --server pjmicrostore-sql --elastic-pool pjmicrostore-sql-sp
 az sql db create --name Products.Photos --resource-group pjmicrostore --server pjmicrostore-sql --elastic-pool pjmicrostore-sql-sp
 
-# DB
 kubectl create secret generic db-user-pass --from-literal=db_username=$sqlUserName --from-literal=db_password="$sqlPassword" --from-literal=db_server=pjmicrostore-sql.database.windows.net -n checkout
 kubectl create secret generic db-user-pass --from-literal=db_username=$sqlUserName --from-literal=db_password="$sqlPassword" --from-literal=db_server=pjmicrostore-sql.database.windows.net -n common
 kubectl create secret generic db-user-pass --from-literal=db_username=$sqlUserName --from-literal=db_password="$sqlPassword" --from-literal=db_server=pjmicrostore-sql.database.windows.net -n external
--- azure/4_Storage.ps1
+++ azure/4_Storage.ps1
@@ -2,6 +2,6 @@ az storage account create -n pjmicrostorestorage -g pjmicrostore -l westeurope -
 
 az storage container create --name 'productsphotos' --account-name pjmicrostorestorage --public-access blob
 
-$connectionString = "(az storage account show-connection-string -g pjmicrostore -n pjmicrostorestorage | ConvertFrom-Json).connectionString"
+$connectionString = "$((az storage account show-connection-string -g pjmicrostore -n pjmicrostorestorage | ConvertFrom-Json).connectionString)"
 
 kubectl create secret generic photo-storage --from-literal=photo_storage=$connectionString -n products

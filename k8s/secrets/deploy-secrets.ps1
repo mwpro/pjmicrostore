20f694fe0649c87156388711fcbee207fc405541
Maciej Wódke <maciej@mwpro.pl>
2019-08-12 02:36:16 +0200

    Db details from secret, persistent sql in local k8s

---
+++ k8s/secrets/deploy-secrets.ps1
@@ -0,0 +1,6 @@
+kubectl create secret generic db-user-pass --from-file=./db_username --from-file=./db_password --from-file=./db_server -n checkout
+kubectl create secret generic db-user-pass --from-file=./db_username --from-file=./db_password --from-file=./db_server -n common
+kubectl create secret generic db-user-pass --from-file=./db_username --from-file=./db_password --from-file=./db_server -n external
+kubectl create secret generic db-user-pass --from-file=./db_username --from-file=./db_password --from-file=./db_server -n identity
+kubectl create secret generic db-user-pass --from-file=./db_username --from-file=./db_password --from-file=./db_server -n products
+kubectl create secret generic db-user-pass --from-file=./db_username --from-file=./db_password --from-file=./db_server -n infrastructure

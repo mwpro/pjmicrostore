de964b3319017d13f06be74d68de5e3311654b8c
Maciej Wódke <maciej@mwpro.pl>
2019-09-11 22:26:35 +0200

    Copy package-lock.json to building dockerfile to fix bugs

--- release-all.ps1
+++ release-all.ps1
@@ -55,11 +55,11 @@ docker push $DOCKER_REPOSITORY_NAME/pjmicrostore-products-photos:$DOCKER_IMAGE_V
 docker push $DOCKER_REPOSITORY_NAME/pjmicrostore-products-photos:latest
 
 # frontend
-$DOCKER_IMAGE_VERSION = "1.0.1"
+$DOCKER_IMAGE_VERSION = "1.0.2"
 docker tag $DOCKER_REPOSITORY_NAME/pjmicrostore-front-admin $DOCKER_REPOSITORY_NAME/pjmicrostore-front-admin:$DOCKER_IMAGE_VERSION
 docker push $DOCKER_REPOSITORY_NAME/pjmicrostore-front-admin:$DOCKER_IMAGE_VERSION
 docker push $DOCKER_REPOSITORY_NAME/pjmicrostore-front-admin:latest
-$DOCKER_IMAGE_VERSION = "1.0.1"
+$DOCKER_IMAGE_VERSION = "1.0.2"
 docker tag $DOCKER_REPOSITORY_NAME/pjmicrostore-front-store $DOCKER_REPOSITORY_NAME/pjmicrostore-front-store:$DOCKER_IMAGE_VERSION
 docker push $DOCKER_REPOSITORY_NAME/pjmicrostore-front-store:$DOCKER_IMAGE_VERSION
 docker push $DOCKER_REPOSITORY_NAME/pjmicrostore-front-store:latest
